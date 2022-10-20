0f69b85eb0da34e06e34162d3148ab06ce2cc0c6
Lyn Yi <Cheng Lyn Yi>
2022-10-20 15:22:26 +0100

    Done

--- primes.py
+++ primes.py
@@ -1,6 +1,17 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(num): 
+    for i in range(2, num):
+        if (num%i==0): 
+            return False
+    return True
+
 def primes(number_of_primes):
     list = []
+    i = 2
+    while len(list) < number_of_primes: 
+        if isPrime(i): 
+            list.append(i); 
+        i= i + 1
     return list
